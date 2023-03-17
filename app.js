912c661b613376b344d4d5b1bbe4018549e721d3
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-03-17 19:52:52 -0400

    fixing login

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").__express);
 
+
+
 router.get("/", function(req,res){
     
     res.render("index", {pagename:"Home"});
@@ -27,24 +29,63 @@ router.get("/about", function(req,res){
     
 })
 
-router.post("/login",function(req,res){
+router.post("/login", function(req, res) {
     console.log(req.body.email);
     console.log(req.body.password);
-    var errors=[];
-    if(req.body.email ==""){
+    var errors = [];
+    if (req.body.email == "") {
         errors.push("Email is required")
     }
-    if(req.body.password ==""){
+    if (req.body.password == "") {
         errors.push("Password is required")
     }
-    if(!/^\w+([\.-]?\w+)+@\w+([|/-]?\w+)+(\.\w{2,3})+$/.test(req.body.email)){
-        errors.push("email is not valid")
+    if (req.body.email !== "mike@aol.com") {
+        errors.push("Invalid email")
     }
-    if(!/^[a-zA-Z]\w{3,14}$/.test(req.body.password)){
-        errors.push("password is not valid")
+    if (req.body.password !== "abc123") {
+        errors.push("Invalid password");
     }
-    res.redirect("/");
+    if (errors.length > 0) {
+        res.render("login", { errors: errors });
+    } else {
+        res.redirect("/profile");
+    }
 })
+
+
+// router.get("/register", function(req, res) {
+//     res.render("register");
+//   });
+  
+//   router.post("/register", function(req, res) {
+//     var errors = [];
+//     var { name, email, password, confirm_password } = req.body;
+  
+//     if (!name) {
+//       errors.push("Name is required");
+//     }
+    
+//     if (!email) {
+//       errors.push("Email is required");
+//     } else if (email !== "mike@aol.com") {
+//       errors.push("Invalid email");
+//     }
+  
+//     if (!password) {
+//       errors.push("Password is required");
+//     } else if (password !== "abc123") {
+//       errors.push("Invalid password");
+//     }
+  
+//     if (errors.length === 0) {
+//       // Save user data to database
+//       res.redirect("/profile");
+//     } else {
+//       res.render("register", { errors: errors });
+//     }
+//   });
+  
+
 
 
 app.use(express.static("public"))
