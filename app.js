c8c127457d1da6b408684a4f3f2029f6000f6952
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-03-17 21:53:42 -0400

    setup session

--- app.js
+++ app.js
@@ -8,30 +8,40 @@ let request = require("request");
 let bodyParser = require("body-parser");
 let ejs = require("ejs");
 
-const router = express.Router();
+const session = require('express-session');
 let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").__express);
 
 
-
 router.get("/", function(req,res){
-    
+    var pagename = "Home";
     res.render("index", {pagename:"Home"});
-
-})
+});
 
 router.get("/about", function(req,res){
-
+    var pagename = "About";
     res.render("about", {pagename:"About"});
-    
-})
+});
 
 router.get("/profile", function(req,res){
-    res.render("profile", {pagename:"Profile"})
-})
+    var pagename = "Profile"
+    res.render("profile", {pagename:"Profile"});
+});
 
 router.post("/login", function(req, res) {
     console.log(req.body.email);
@@ -51,10 +61,23 @@ router.post("/login", function(req, res) {
     }
     if (errors.length > 0) {
         res.render("login", { errors: errors });
+    } 
+    if (req.body.email === 'Mike@aol.com' && req.body.password === 'abc123') {
+        req.session.loggedin = true;
+        res.render('profile');
     } else {
-        res.redirect("/profile");
+        req.session.loggedin = false;
+        res.render('index');
     }
-})
+});
+
+app.use(express.static("public"));
+app.use("/", router);
+
+var server = app.listen("8080");
+
+
+
 
 
 // router.get("/register", function(req, res) {
@@ -88,11 +111,3 @@ router.post("/login", function(req, res) {
 //       res.render("register", { errors: errors });
 //     }
 //   });
-  
-
-
-
-app.use(express.static("public"))
-app.use("/", router);
-var server = app.listen("8080");
-
